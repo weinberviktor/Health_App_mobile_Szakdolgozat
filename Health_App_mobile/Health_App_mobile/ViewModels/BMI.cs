4124205487d07fdd1629ef6763db63b4ea9fdaeb
vikto <vikto@DESKTOP-8O7VMPB>
2021-04-07 17:46:52 +0200

    BMI kalulator

---
+++ Health_App_mobile/Health_App_mobile/ViewModels/BMI.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Health_App_mobile.ViewModels
+{
+    internal class BMI : INotifyPropertyChanged
+    {
+        private double magassag = 180;
+        private double testsuly = 72;
+        private const double bmiindex = 1.0;
+
+        public double Magassag
+        {
+            get => magassag;
+            set
+            {
+                magassag = NextStep(value);
+                Eredmenyfrissitese();
+            }
+        }
+
+        public double Testsuly
+        {
+            get => testsuly;
+            set
+            {
+                testsuly = NextStep(value);
+                Eredmenyfrissitese();
+            }
+        }
+
+        public double Bmi
+            => Math.Round(Testsuly / Math.Pow(Magassag / 100, 2), 2);
+
+        public string Osztalyozas
+        {
+            get
+            {
+                if (Bmi < 18.5)
+                    return "Soványság";
+                else if (Bmi < 25)
+                    return "Normál testsúly";
+                else if (Bmi < 30)
+                    return "Túlsúlyos";
+                else
+                    return "Elhízott";
+            }
+        }
+
+        private void Eredmenyfrissitese()
+        {
+            Valtozas(nameof(Bmi));
+            Valtozas(nameof(Osztalyozas));
+        }
+
+        private double NextStep(double value)
+            => Math.Round(value / bmiindex) * bmiindex;
+
+        private void Valtozas(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Health_App_mobile/Health_App_mobile/Views/BmiCalculator.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Health_App_mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Health_App_mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BmiCalculator : ContentPage
+    {
+        public BmiCalculator()
+        {
+            InitializeComponent();
+            BindingContext = new BMI();
+        }
+    }
+}
