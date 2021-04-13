5e79a994330726f5af1bf3ebd07ef1044525c6f4
vikto <vikto@DESKTOP-8O7VMPB>
2021-04-13 19:43:46 +0200

    Kaloria

--- Health_App_mobile/Health_App_mobile/Views/Activity.xaml.cs
+++ Health_App_mobile/Health_App_mobile/Views/Activity.xaml.cs
@@ -12,22 +12,37 @@ namespace Health_App_mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Activity : ContentPage
     {
+        public IList<Etelek> Eteleklista { get; set; }
         public Activity()
         {
             InitializeComponent();
+            Eteleklista = new List<Etelek>();
+            Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria="60 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria = "60 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria = "60 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria = "60 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria = "60 kcal" });
 
+            BindingContext = this;
         }
+
+        public class Etelek
+        {
+            public string Etelneve { get; set; }
+            public string Etelemennyiseg { get; set; }
+            public string Etelekaloria { get; set; }
+        }
+
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            Application.Current.Properties["Szoveg"] = etel.Text;
+            //Application.Current.Properties["Szoveg"] = etel.Text;
             DisplayAlert("Étel felírva","Eltároltuk az ételed erre a napra","Ok");
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
-            etel.Text = string.Empty;
+            //etel.Text = string.Empty;
         }
-
     }
 }
