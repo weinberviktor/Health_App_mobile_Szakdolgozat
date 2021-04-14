ad65926e6f5f3fe31d2f34f8ecd2e1e54a975a6e
vikto <vikto@DESKTOP-8O7VMPB>
2021-04-14 17:47:00 +0200

    etelek

--- Health_App_mobile/Health_App_mobile/Views/Activity.xaml.cs
+++ Health_App_mobile/Health_App_mobile/Views/Activity.xaml.cs
@@ -34,7 +34,18 @@ namespace Health_App_mobile.Views
             Eteleklista.Add(new Etelek { Etelneve = "Bableves", Etelemennyiseg = "1 tányér", Etelekaloria = "384 kcal" });
             Eteleklista.Add(new Etelek { Etelneve = "Gyömölcsleves", Etelemennyiseg = "1 tányér", Etelekaloria = "245 kcal" });
             Eteleklista.Add(new Etelek { Etelneve = "Zöldborsóleves", Etelemennyiseg = "1 tányér", Etelekaloria = "198 kcal" });
-
+            Eteleklista.Add(new Etelek { Etelneve = "Alma", Etelemennyiseg = "1 db", Etelekaloria = "95 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Pestos tészta", Etelemennyiseg = "1 adag/270 g", Etelekaloria = "931 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Rakott Krumpli", Etelemennyiseg = "1 adag/400 g", Etelekaloria = "50 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Rántott hús", Etelemennyiseg = "70 g", Etelekaloria = "147 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Főtt fehér rizs", Etelemennyiseg = "300 g", Etelekaloria = "390 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Főtt krumpli", Etelemennyiseg = "1 adag/300 g", Etelekaloria = "231 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Sült krumpli", Etelemennyiseg = "117 g", Etelekaloria = "342 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Görörg Saláta", Etelemennyiseg = "1 adag/250 g", Etelekaloria = "180 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Cézár Saláta", Etelemennyiseg = "1 adag/250 g", Etelekaloria = "288 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Paradicsomos tészta", Etelemennyiseg = "1 adag/400 g", Etelekaloria = "318 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Sajtal sonkávaL töltött csirkemell", Etelemennyiseg = "1 adag/200 g", Etelekaloria = "480 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Marha bélszín steak", Etelemennyiseg = "1 adag/150 g", Etelekaloria = "308 kcal" });
 
             BindingContext = this;
         }
