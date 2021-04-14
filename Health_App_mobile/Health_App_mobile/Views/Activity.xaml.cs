47da8efe21010d7d23b87b2d2b838d4e636d2e15
vikto <vikto@DESKTOP-8O7VMPB>
2021-04-14 11:36:47 +0200

    Etelek

--- Health_App_mobile/Health_App_mobile/Views/Activity.xaml.cs
+++ Health_App_mobile/Health_App_mobile/Views/Activity.xaml.cs
@@ -12,22 +12,25 @@ namespace Health_App_mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Activity : ContentPage
     {
+
         public IList<Etelek> Eteleklista { get; set; }
         public Activity()
         {
             InitializeComponent();
             Eteleklista = new List<Etelek>();
             Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "60g/szelet", Etelekaloria="180 kcal" });
-            Eteleklista.Add(new Etelek { Etelneve = "Barna kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria = "60 kcal" });
-            Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria = "60 kcal" });
-            Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria = "60 kcal" });
-            Eteleklista.Add(new Etelek { Etelneve = "Fehér kenyér", Etelemennyiseg = "180g/szelet", Etelekaloria = "60 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Barna kenyér", Etelemennyiseg = "30g/szelet", Etelekaloria = "75 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Zsömle", Etelemennyiseg = "54g/db", Etelekaloria = "149 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Kifli", Etelemennyiseg = "44g/db", Etelekaloria = "120 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Vaj", Etelemennyiseg = "10g", Etelekaloria = "72 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Csirkemell sonka", Etelemennyiseg = "1 szelet", Etelekaloria = "12 kcal" });
+            Eteleklista.Add(new Etelek { Etelneve = "Téliszalámi", Etelemennyiseg = "10 g", Etelekaloria = "50 kcal" });
 
             BindingContext = this;
         }
 
         public class Etelek
-        {
+        {   
             public string Etelneve { get; set; }
             public string Etelemennyiseg { get; set; }
             public string Etelekaloria { get; set; }
@@ -36,23 +39,13 @@ namespace Health_App_mobile.Views
         private void Button_Clicked_2(object sender, EventArgs e)
         {
             //osszkaloria.Text = ($"{Eteleklista.Count}"+ "napi kalória");
+            //osszkaloria.Text = (nameof(Etelek));
+            
         }
-
         private void Kereso_TextChanged(object sender, TextChangedEventArgs e)
         {
             var kereseserdemeny = Eteleklista.Where(k => k.Etelneve.ToLower().Contains(kereso.Text.ToLower()));
             listview.ItemsSource = kereseserdemeny;
         }
-
-        //private void Button_Clicked(object sender, EventArgs e)
-        //{
-        //    //Application.Current.Properties["Szoveg"] = etel.Text;
-        //    DisplayAlert("Étel felírva","Eltároltuk az ételed erre a napra","Ok");
-        //}
-
-        //private void Button_Clicked_1(object sender, EventArgs e)
-        //{
-        //    //etel.Text = string.Empty;
-        //} 
     }
 }
